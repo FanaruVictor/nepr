9fe647330f78ce400d671ba72afb261ec87a2934
Fanaru Victor-Daniel <victor.fanaru@centric.eu>
2025-01-26 22:04:12 +0200

    remove axios

--- nepr/src/app/sparql/page.tsx
+++ nepr/src/app/sparql/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { useState } from 'react';
-import axios from 'axios';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 
 export default function SparqlPage() {
